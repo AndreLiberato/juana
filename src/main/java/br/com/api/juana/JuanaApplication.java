c5703a757ecc08b523885162d81a5ce8647257c5
André Liberato <andre.liberato@outlook.com.br>
2023-09-26 14:58:28 -0300

    Primeiro commit

---
+++ src/main/java/br/com/api/juana/JuanaApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.juana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/juana/JuanaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.juana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
