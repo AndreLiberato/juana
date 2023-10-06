297736c0c652e95840e447901d0e8b805b6db224
André Liberato <andre.liberato@outlook.com.br>
2023-10-06 18:09:23 -0300

    Reformulação de alguns métodos e implementação do PessoaController

--- src/main/java/br/com/api/juana/controllers/pessoas/PacienteController.java
+++ src/main/java/br/com/api/juana/controllers/pessoas/PacienteController.java
@@ -1,8 +1,9 @@
-package br.com.api.juana.controllers.usuarios;
+package br.com.api.juana.controllers.pessoas;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +15,29 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.juana.mappers.pessoas.PacienteMapper;
+import br.com.api.juana.models.PessoaModel;
 import br.com.api.juana.models.pessoas.PacienteModel;
 import br.com.api.juana.payloads.pessoas.PacientePayload;
+import br.com.api.juana.services.PessoaService;
 import br.com.api.juana.services.pessoas.PacienteService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/paciente")
+@RequestMapping("/pessoa/paciente")
 public class PacienteController {
 	@Autowired
 	private PacienteService pacienteService;
 
+	@Autowired
+	private PessoaService pessoaService;
+
+	@Autowired
+	private PacienteMapper pacienteMapper;
+
 	@GetMapping
-	public ResponseEntity<Object> index() {
+	public ResponseEntity<?> index() {
 		List<PacienteModel> pacientes = pacienteService.getAll();
 		if (pacientes.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum paciente cadastrado!");
@@ -35,40 +46,47 @@ public class PacienteController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Object> show(@PathVariable("id") UUID id) {
-		Optional<PacienteModel> resposta = pacienteService.findOne(id);
-		if (resposta.isEmpty()) {
+	public ResponseEntity<?> show(@PathVariable("id") UUID id) {
+		Optional<PacienteModel> paciente = pacienteService.findOne(id);
+		if (paciente.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Paciente não encontrado!");
 		}
-		return ResponseEntity.status(HttpStatus.OK).body(resposta.get());
+		return ResponseEntity.status(HttpStatus.OK).body(paciente.get());
 	}
 
 	@PostMapping
-	public ResponseEntity<PacienteModel> store(@Valid @RequestBody PacientePayload pacientePayload) {
-		PacienteModel paciente = new PacienteModel(pacientePayload.getNomeCompleto(),
-				pacientePayload.getDataNascimento(), pacientePayload.getNomeSocial());
+	public ResponseEntity<?> store(@Valid @RequestBody PacientePayload pacientePayload) {
+		PacienteModel paciente = pacienteMapper.apply(pacientePayload);
+		Optional<PessoaModel> pessoa = pessoaService.findOne(pacientePayload.getPessoaId());
+		if (pessoa.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O paciente não possui pessoa associada!");
+		}
+		paciente.setPessoa(pessoa.get());
 		return ResponseEntity.status(HttpStatus.CREATED).body(pacienteService.createEntity(paciente));
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<Object> update(@Valid @RequestBody PacientePayload pacientePayload,
-			@PathVariable("id") UUID id) {
-		PacienteModel paciente = new PacienteModel(pacientePayload.getNomeCompleto(),
-				pacientePayload.getDataNascimento(), pacientePayload.getNomeSocial());
-		Optional<PacienteModel> resposta = pacienteService.updateEntity(paciente, id);
-		if (resposta.isEmpty()) {
+	public ResponseEntity<?> update(@Valid @RequestBody PacientePayload pacientePayload, @PathVariable("id") UUID id) {
+		PacienteModel paciente = pacienteMapper.apply(pacientePayload);
+		Optional<PessoaModel> pessoa = pessoaService.findOne(pacientePayload.getPessoaId());
+		if (pessoa.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O paciente não possui pessoa associada!");
+		}
+		paciente.setPessoa(pessoa.get());
+		Optional<PacienteModel> result = pacienteService.updateEntity(paciente, id);
+		if (result.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Paciente não encontrado!");
 		}
-		return ResponseEntity.status(HttpStatus.OK).body(resposta.get());
+		return ResponseEntity.status(HttpStatus.OK).body(result.get());
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Object> delete(@PathVariable("id") UUID id) {
-		Optional<PacienteModel> resposta = pacienteService.findOne(id);
-		if (resposta.isEmpty()) {
+	public ResponseEntity<?> delete(@PathVariable("id") UUID id) {
+		Optional<PacienteModel> paciente = pacienteService.findOne(id);
+		if (paciente.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Paciente não encontrado!");
 		}
-		pacienteService.deleteEntity(resposta.get());
+		pacienteService.deleteEntity(paciente.get());
 		return ResponseEntity.status(HttpStatus.OK).body("Paciente excluído!");
 	}
 }
--- src/main/java/br/com/api/juana/controllers/pessoas/PrescritorController.java
+++ src/main/java/br/com/api/juana/controllers/pessoas/PrescritorController.java
@@ -1,8 +1,9 @@
-package br.com.api.juana.controllers.usuarios;
+package br.com.api.juana.controllers.pessoas;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,61 +15,79 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.juana.mappers.pessoas.PrescritorMapper;
+import br.com.api.juana.models.PessoaModel;
 import br.com.api.juana.models.pessoas.PrescritorModel;
 import br.com.api.juana.payloads.pessoas.PrescritorPayload;
+import br.com.api.juana.services.PessoaService;
 import br.com.api.juana.services.pessoas.PrescritorService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/prescritor")
+@RequestMapping("/pessoa/prescritor")
 public class PrescritorController {
 	@Autowired
 	private PrescritorService prescritorService;
 
+	@Autowired
+	private PessoaService pessoaService;
+
+	@Autowired
+	private PrescritorMapper prescritorMapper;
+
 	@GetMapping
-	public ResponseEntity<Object> index() {
-		List<PrescritorModel> pacientes = prescritorService.getAll();
-		if (pacientes.isEmpty()) {
+	public ResponseEntity<?> index() {
+		List<PrescritorModel> prescritores = prescritorService.getAll();
+		if (prescritores.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum prescritor cadastrado!");
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(prescritorService.getAll());
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Object> show(@PathVariable("id") UUID id) {
-		Optional<PrescritorModel> resposta = prescritorService.findOne(id);
-		if (resposta.isEmpty()) {
+	public ResponseEntity<?> show(@PathVariable("id") UUID id) {
+		Optional<PrescritorModel> prescritor = prescritorService.findOne(id);
+		if (prescritor.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Prescitor não encontrado!");
 		}
-		return ResponseEntity.status(HttpStatus.OK).body(resposta.get());
+		return ResponseEntity.status(HttpStatus.OK).body(prescritor.get());
 	}
 
 	@PostMapping
-	public ResponseEntity<PrescritorModel> store(@Valid @RequestBody PrescritorPayload prescritorPayload) {
-		PrescritorModel paciente = new PrescritorModel(prescritorPayload.getNomeCompleto(),
-				prescritorPayload.getDataNascimento(), prescritorPayload.getNumeroRegistro());
-		return ResponseEntity.status(HttpStatus.CREATED).body(prescritorService.createEntity(paciente));
+	public ResponseEntity<?> store(@Valid @RequestBody PrescritorPayload prescritorPayload) {
+		PrescritorModel prescritor = prescritorMapper.apply(prescritorPayload);
+		Optional<PessoaModel> pessoa = pessoaService.findOne(prescritorPayload.getPessoaId());
+		if (pessoa.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O paciente não possui pessoa associada!");
+		}
+		prescritor.setPessoa(pessoa.get());
+		return ResponseEntity.status(HttpStatus.CREATED).body(prescritorService.createEntity(prescritor));
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<Object> update(@Valid @RequestBody PrescritorPayload prescritorPayload,
+	public ResponseEntity<?> update(@Valid @RequestBody PrescritorPayload prescritorPayload,
 			@PathVariable("id") UUID id) {
-		PrescritorModel paciente = new PrescritorModel(prescritorPayload.getNomeCompleto(),
-				prescritorPayload.getDataNascimento(), prescritorPayload.getNumeroRegistro());
-		Optional<PrescritorModel> resposta = prescritorService.updateEntity(paciente, id);
-		if (resposta.isEmpty()) {
+		PrescritorModel prescritor = prescritorMapper.apply(prescritorPayload);
+		Optional<PessoaModel> pessoa = pessoaService.findOne(prescritorPayload.getPessoaId());
+		if (pessoa.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O prescritor não possui pessoa associada!");
+		}
+		prescritor.setPessoa(pessoa.get());
+		Optional<PrescritorModel> result = prescritorService.updateEntity(prescritor, id);
+		if (result.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Prescritor não encontrado!");
 		}
-		return ResponseEntity.status(HttpStatus.OK).body(resposta.get());
+		return ResponseEntity.status(HttpStatus.OK).body(result.get());
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Object> delete(@PathVariable("id") UUID id) {
-		Optional<PrescritorModel> resposta = prescritorService.findOne(id);
-		if (resposta.isEmpty()) {
+	public ResponseEntity<?> delete(@PathVariable("id") UUID id) {
+		Optional<PrescritorModel> prescritor = prescritorService.findOne(id);
+		if (prescritor.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Prescritor não encontrado!");
 		}
-		prescritorService.deleteEntity(resposta.get());
+		prescritorService.deleteEntity(prescritor.get());
 		return ResponseEntity.status(HttpStatus.OK).body("Prescritor excluído!");
 	}
 }
