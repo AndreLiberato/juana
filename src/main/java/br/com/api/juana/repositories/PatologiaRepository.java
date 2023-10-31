d6be187f7f3e9f860f93fa84fa0efa453bb272b3
André Liberato <andre.liberato@outlook.com.br>
2023-10-31 09:44:09 -0300

    Repositories das novas entidades do sistema (#10)

--- src/main/java/br/com/api/juana/repositories/PatologiaRepository.java
+++ src/main/java/br/com/api/juana/repositories/PatologiaRepository.java
@@ -5,9 +5,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.api.juana.models.PessoaModel;
+import br.com.api.juana.models.PatologiaModel;
 
 @Repository
-public interface PessoaRepository extends JpaRepository<PessoaModel, UUID> {
+public interface PatologiaRepository extends JpaRepository<PatologiaModel, UUID> {
 
 }
