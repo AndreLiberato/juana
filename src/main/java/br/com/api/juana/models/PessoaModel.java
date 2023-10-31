7fe056faa404e4e13ccf02530a8d17d7a21943cb
André Liberato <andre.liberato@outlook.com.br>
2023-10-31 09:32:12 -0300

    Adição e ajustes nos atributos de pessoas e tipos de pessoas (#14)

--- src/main/java/br/com/api/juana/models/PessoaModel.java
+++ src/main/java/br/com/api/juana/models/PessoaModel.java
@@ -1,60 +1,77 @@
 package br.com.api.juana.models;
 
-import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Objects;
-import java.util.UUID;
 
+import br.com.api.juana.enums.Genero;
+import br.com.api.juana.enums.UnidadeFederativa;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "pessoa")
-public class PessoaModel implements Serializable {
+public class PessoaModel extends EntidadeModel {
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.UUID)
-	private UUID id;
 
 	@Column(name = "nome_completo", nullable = false, length = 128)
 	private String nomeCompleto;
 
+	@Column(name = "cpf", nullable = false, length = 11, unique = true)
+	private String cpf;
+
+	@Column(name = "email", nullable = false, length = 64, unique = true)
+	private String email;
+
+	@Column(name = "senha", nullable = false)
+	private String senha;
+
+	@Column(name = "numero_registro_geral", length = 8, unique = true)
+	private String numeroRegistroGeral;
+
+	@Column(name = "nomeExpedidorRegistroGeral", length = 16)
+	private String nomeExpedidorRegistroGeral;
+
+	@Column(name = "unidade_federativa_registro_geral", length = 32)
+	@Enumerated(EnumType.STRING)
+	private UnidadeFederativa unidadeFederativaRegistroGeral;
+
+	@Column(name = "genero", nullable = false, length = 16)
+	@Enumerated(EnumType.STRING)
+	private Genero genero;
+
+	@Column(name = "unidade_federativa_nascimento", nullable = false, length = 32)
+	@Enumerated(EnumType.STRING)
+	private UnidadeFederativa unidadeFederativaNascimento;
+
+	@Column(name = "cidade_nascimento", nullable = false, length = 64)
+	private String cidadeNascimento;
+
 	@Column(name = "data_nascimento", nullable = false)
 	private LocalDate dataNascimento;
 
-	@Column(name = "criado_em", nullable = false, updatable = false)
-	private LocalDateTime criadoEm;
-
-	@Column(name = "editado_em", nullable = false)
-	private LocalDateTime editadoEm;
-
 	public PessoaModel() {
-		this.criadoEm = this.editadoEm = LocalDateTime.now();
+		super();
 	}
 
-	public PessoaModel(String nomeCompleto, LocalDate dataNascimento) {
+	public PessoaModel(String nomeCompleto, String cpf, String email, String senha, String numeroRegistroGeral,
+			String nomeExpedidorRegistroGeral, UnidadeFederativa unidadeFederativaRegistroGeral, Genero genero,
+			UnidadeFederativa unidadeFederativaNascimento, String cidadeNascimento, LocalDate dataNascimento,
+			LocalDateTime criadoEm, LocalDateTime editadoEm) {
+		super();
 		this.nomeCompleto = nomeCompleto;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		this.numeroRegistroGeral = numeroRegistroGeral;
+		this.nomeExpedidorRegistroGeral = nomeExpedidorRegistroGeral;
+		this.unidadeFederativaRegistroGeral = unidadeFederativaRegistroGeral;
+		this.genero = genero;
+		this.unidadeFederativaNascimento = unidadeFederativaNascimento;
+		this.cidadeNascimento = cidadeNascimento;
 		this.dataNascimento = dataNascimento;
-		this.criadoEm = this.editadoEm = LocalDateTime.now();
-	}
-
-	public PessoaModel(UUID id) {
-		this.id = id;
-		this.criadoEm = this.editadoEm = LocalDateTime.now();
-	}
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
 	}
 
 	public String getNomeCompleto() {
@@ -65,6 +82,78 @@ public class PessoaModel implements Serializable {
 		this.nomeCompleto = nomeCompleto;
 	}
 
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNumeroRegistroGeral() {
+		return numeroRegistroGeral;
+	}
+
+	public void setNumeroRegistroGeral(String numeroRegistroGeral) {
+		this.numeroRegistroGeral = numeroRegistroGeral;
+	}
+
+	public String getNomeExpedidorRegistroGeral() {
+		return nomeExpedidorRegistroGeral;
+	}
+
+	public void setNomeExpedidorRegistroGeral(String nomeExpedidorRegistroGeral) {
+		this.nomeExpedidorRegistroGeral = nomeExpedidorRegistroGeral;
+	}
+
+	public UnidadeFederativa getUnidadeFederativaRegistroGeral() {
+		return unidadeFederativaRegistroGeral;
+	}
+
+	public void setUnidadeFederativaRegistroGeral(UnidadeFederativa unidadeFederativaRegistroGeral) {
+		this.unidadeFederativaRegistroGeral = unidadeFederativaRegistroGeral;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+	public UnidadeFederativa getUnidadeFederativaNascimento() {
+		return unidadeFederativaNascimento;
+	}
+
+	public void setUnidadeFederativaNascimento(UnidadeFederativa unidadeFederativaNascimento) {
+		this.unidadeFederativaNascimento = unidadeFederativaNascimento;
+	}
+
+	public String getCidadeNascimento() {
+		return cidadeNascimento;
+	}
+
+	public void setCidadeNascimento(String cidadeNascimento) {
+		this.cidadeNascimento = cidadeNascimento;
+	}
+
 	public LocalDate getDataNascimento() {
 		return dataNascimento;
 	}
@@ -73,43 +162,14 @@ public class PessoaModel implements Serializable {
 		this.dataNascimento = dataNascimento;
 	}
 
-	public LocalDateTime getCriadoEm() {
-		return criadoEm;
-	}
-
-	public void setCriadoEm(LocalDateTime criadoEm) {
-		this.criadoEm = criadoEm;
-	}
-
-	public LocalDateTime getEditadoEm() {
-		return editadoEm;
-	}
-
-	public void setEditadoEm(LocalDateTime editadoEm) {
-		this.editadoEm = editadoEm;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PessoaModel other = (PessoaModel) obj;
-		return Objects.equals(id, other.id);
-	}
-
 	@Override
 	public String toString() {
-		return "PessoaModel [id=" + id + ", nomeCompleto=" + nomeCompleto + ", dataNascimento=" + dataNascimento
-				+ ", criadoEm=" + criadoEm + ", editadoEm=" + editadoEm + "]";
+		return "PessoaModel [id=" + id + ", criadoEm=" + criadoEm + ", editadoEm=" + editadoEm + ", nomeCompleto="
+				+ nomeCompleto + ", cpf=" + cpf + ", email=" + email + ", senha=" + senha + ", numeroRegistroGeral="
+				+ numeroRegistroGeral + ", nomeExpedidorRegistroGeral=" + nomeExpedidorRegistroGeral
+				+ ", unidadeFederativaRegistroGeral=" + unidadeFederativaRegistroGeral + ", genero=" + genero
+				+ ", unidadeFederativaNascimento=" + unidadeFederativaNascimento + ", cidadeNascimento="
+				+ cidadeNascimento + ", dataNascimento=" + dataNascimento + "]";
 	}
 
 }
